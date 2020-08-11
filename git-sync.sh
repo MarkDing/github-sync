8aabeee018a0dd68fd8e9acf9d285e94afa7f2be
silabs-JimL <jim.lin@silabs.com>
2020-08-11 14:17:50 +0800

    tt

--- git-sync.sh
+++ git-sync.sh
@@ -35,22 +35,19 @@ echo "SOURCE=$SOURCE_REPO:$SOURCE_BRANCH"
 echo "DESTINATION=$DESTINATION_REPO:$DESTINATION_BRANCH"
 
 git clone "$SOURCE_REPO" /root/source --origin source && cd /root/source
-chmod 777 gen-files.sh
-./gen-files.sh
-git add .
-echo "-----1"
-git config --global user.name ${GIT_USER}
-echo "-----2"
-# git config --global user.password ${GIT_PASSWD}
-git config --global user.email ${GIT_EMAIL}
-echo "-----3"
-git commit -a -m "gen links"
-echo "-----4"
 git remote add destination "$DESTINATION_REPO"
-echo "-----4 $DESTINATION_REPO"
 
 # Pull all branches references down locally so subsequent commands can see them
 git fetch source '+refs/heads/*:refs/heads/*' --update-head-ok
+
+chmod 777 gen-files.sh
+./gen-files.sh
+git add .
+git config --global user.name ${GIT_USER}
+# git config --global user.password ${GIT_PASSWD}
+git config --global user.email ${GIT_EMAIL}
+git commit -a -m "gen links"
+
 
 # Print out all branches
 git --no-pager branch -a -vv
